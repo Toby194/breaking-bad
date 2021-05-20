c5e94c919b0a844a56eb2fc8b4c94207ed4a3a0c
nema <ShiYash@Tenzings-Air.attlocal.net>
2021-05-20 15:23:39 -0700

    last commit

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<header className='center'>
+			<img
+				src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Breaking_Bad_logo.svg/1024px-Breaking_Bad_logo.svg.png'
+				alt='logo'
+			/>
+		</header>
+	);
+}
+
+export default Header;
